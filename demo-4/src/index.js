f9f0c36d4161d3b2549f03937b7dbed9125dcba6
fox <1025687605@qq.com>
2021-04-19 11:04:20 +0800

    add

--- demo-4/src/index.js
+++ demo-4/src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
 
 function component() {
   const element = document.createElement('div')
 
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
+
+  // add image
+  const myIcon = new Image()
 
   return element
 }
