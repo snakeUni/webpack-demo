befd9f30ff678519097c58811a8682db159f7c44
fox <1025687605@qq.com>
2021-04-21 23:12:43 +0800

    add

---
+++ demo-8-library/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webpack-numbers.js',
+    // 配置项暴露从入口导出的内容
+    library: {
+      name: 'webpackNumbers',
+      type: 'umd'
+    }
+  },
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_'
+    }
+  }
+}
